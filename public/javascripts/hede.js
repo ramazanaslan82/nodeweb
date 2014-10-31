1c6feef1c70c23ed09e5515c8291307204b064cc
Ramazan ASLAN <ramazan.aslan@sahibinden.com>
2014-10-31 10:59:03 +0200

    basic register form added

---
+++ public/javascripts/hede.js
@@ -0,0 +1,5 @@
+/**
+ * Created by ramazana on 30/10/14.
+ */
+alert("hede");
+console.log("hoppa");
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/asd', function(req, res) {
+    res.render('asd', { title: 'My Asd Page' });
+});
+
+router.get('/hello', function(req, res) {
+    res.send("merhaba");
+});
+
+router.get('/magnet', function(req, res) {
+    res.render('magnet_register', {});
+});
+
+router.get('/magnet/uye-ol', function(req, res) {
+    res.render('magnet_register', {});
+});
+
+router.get('/magnet/karakter-sec', function(req, res) {
+    res.render('magnet_choose_character', {});
+});
+
 module.exports = router;
